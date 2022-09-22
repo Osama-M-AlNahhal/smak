9c38a8766ed07d6c7e6e773e1629e194ce6cb57b
Osama MN <81105219+Osama-M-AlNahhal@users.noreply.github.com>
2022-09-22 10:41:16 +0300

    about us section done

---
+++ php/about.php
@@ -0,0 +1,46 @@
+<section class="about-us-section">
+	<div class="about-us-container">
+		<h3 class="au-secondary-title">ABOUT US</h3>
+		<h2 class="au-main-title">WHO WE ARE</h2>
+		<div class="divider"></div>
+		<p class="au-description">We're a sincere company with a straightforward vision. We believe that there is an amazing amount to be learned by working in a "real-world" web production environment, alongside other like-minded developers, to help bring people's imaginations into reality. and with that in mind, the following values define our work, and by extension, define us:</p>
+		<div class="values-wrapper">
+			<div class="value">
+				<div class="value-icon-wrapper">
+					<img src="img/alarm.svg" alt="alarm icon" class="value-icon">
+				</div>
+				<div class="value-text">
+					<h4 class="value-name">ON TIME PROJECTS</h4>
+					<p class="value-description">Your time is valuable to us, and we always work with that in mind, we work to deliver our work on time without any delay.</p>
+				</div>
+			</div>
+			<div class="value">
+				<div class="value-icon-wrapper">
+					<img src="img/lifebuoy.svg" alt="lifebuoy icon" class="value-icon">
+				</div>
+				<div class="value-text">
+					<h4 class="value-name">FULL SUPPORT</h4>
+					<p class="value-description">Not only do we take care of the project from start to finish, we also keep taking care of it after delivery, should the need for any improvements arise, we'll swiftly take care of it.</p>
+				</div>
+			</div>
+			<div class="value">
+				<div class="value-icon-wrapper">
+					<img src="img/thumbs-up.png" alt="thumbs up icon" class="value-icon">
+				</div>
+				<div class="value-text">
+					<h4 class="value-name">PROFESSIONAL EXPERIENCE</h4>
+					<p class="value-description">With over a decade of professional experience under our belts, our team is more than capable of handling any project our clients need.</p>
+				</div>
+			</div>
+			<div class="value">
+				<div class="value-icon-wrapper">
+					<img src="img/bubbles2.svg" alt="speach bubble icon" class="value-icon">
+				</div>
+				<div class="value-text">
+					<h4 class="value-name">COMMUNICATIVE TEAM</h4>
+					<p class="value-description">We keep you in the loop, with regular updates and revisions with the client to assure that the project is exactly how you invision it.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
